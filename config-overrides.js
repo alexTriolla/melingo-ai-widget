0eec15ac474ba91ab9139306b245015959edce9b
alexTriolla <alex@triolla.io>
2024-02-28 13:35:19 +0200

    foo

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+  if (config.output) {
+    config.output.filename = 'static/js/[name].js';
+    config.output.chunkFilename = 'static/js/[name].chunk.js';
+  }
+  config.plugins.forEach((plugin) => {
+    if (plugin.constructor.name === 'MiniCssExtractPlugin') {
+      plugin.options.filename = 'static/css/[name].css';
+      plugin.options.chunkFilename = 'static/css/[name].chunk.css';
+    }
+  });
+  return config;
+};
