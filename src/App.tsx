c427ba6d6063ed706fec97f58952e6182960a723
alexTriolla <alex@triolla.io>
2024-02-28 16:09:12 +0200

    sdfsdfsf

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,28 @@ function App() {
     setIsOpen(!isOpen);
   };
 
-  return (
-      <div className="app-container">
-        {isOpen ? (
-          <div className="widget">
-            <div className="widget-header">
-              <div className="logo-container">
-                <img src="https://via.placeholder.com/50" alt="logo" />
-              </div>
-              <div className="title-container">
-                <p>Chatbot Name</p>
-                <span>Our bot will reply instantly</span>
-              </div>
-              <button className="close-button" onClick={handleOpen}>
-                X
-              </button>
-            </div>
-            <div className="widget-content">
-              <p>Content</p>
-            </div>
-          </div>
-        ) : (
-          <button className="app-button" onClick={handleOpen}>
-            OPEN
-          </button>
-        )}
+  return isOpen ? (
+    <div className="widget">
+      <div className="widget-header">
+        <div className="logo-container">
+          <img src="https://via.placeholder.com/50" alt="logo" />
+        </div>
+        <div className="title-container">
+          <p>Chatbot Name</p>
+          <span>Our bot will reply instantly</span>
+        </div>
+        <button className="close-button" onClick={handleOpen}>
+          X
+        </button>
       </div>
+      <div className="widget-content">
+        <p>Content</p>
+      </div>
+    </div>
+  ) : (
+    <button className="app-button" onClick={handleOpen}>
+      OPEN
+    </button>
   );
 }
 
