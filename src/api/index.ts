dd6fc1703963e96bbebd7c99fd0953dedbe2124b
alexTriolla <alex@triolla.io>
2024-03-05 10:10:07 +0200

    rtl fix, base rl fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,15 +2,13 @@ import axios, { AxiosInstance } from 'axios';
 
 // Create an Axios instance with the baseURL and default headers set
 const baseUrl: AxiosInstance = axios.create({
-  baseURL: 'http://a3c5e9cd58a734f708a2266883d8c41c-287846994.il-central-1.elb.amazonaws.com:8088',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 });
 
-
 baseUrl.defaults.timeout = 180000; // Axios configuration for timeout
-
 
 export default baseUrl;
