30ed17a9dc1174f37c27f27f5df475919905a4f7
alexTriolla <alex@triolla.io>
2024-06-23 15:31:07 +0300

    Refactor API base URL to use environment variable

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
+
 // Create an Axios instance with the baseURL and default headers set
 const baseUrl: AxiosInstance = axios.create({
-  baseURL:
-    'http://meuhedet.dev.melingo.ai:8088',
+  baseURL: process.env.REACT_APP_TITLE,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
